8beec4206bc2615152fc2b7fa0c70e067c9d65e3
lawric1 <67882089+lawric1@users.noreply.github.com>
2021-11-20 22:54:14 -0300

    Add create_gif function

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
 from PIL import Image
 
-file = '3x.gif'
-
 def create_Spritesheet(file):
     frames = []
 
     if file.endswith(".gif"):
-        with Image.open('3x.gif') as im:
-            print(im)
+        with Image.open(file) as im:
             for i in range(im.n_frames):
                 im.seek(i)
                 frames.append(im.convert('RGB'))
@@ -25,4 +22,34 @@ def create_Spritesheet(file):
 
     spriteSheet.save(file[:-4] + ' spritesheet.png')
 
-create_Spritesheet(file)
+def create_GIF(file, frameSize, filename):
+    with Image.open(file) as im:
+        width, height = frameSize
+        left, upper, right, lower = 0, 0, width, height
+
+        frameNumber = im.width // width
+        frames = []
+
+        for i in range(frameNumber):
+            frame = im.crop((left, upper, right, lower))
+            frames.append(frame)
+
+            left  += width
+            upper += 0
+            right += width
+            lower += 0
+        
+        frames[0].save(filename + '.gif', format='GIF', 
+                        append_images=frames[1:], 
+                        save_all=True, 
+                        duration=150, 
+                        loop=0)
+
+# create_Spritesheet(file)
+# create_GIF('3x spritesheet.png', (112, 112), 'export')
+
+# Todo
+    # Get file from sysarg
+    # Define number of rows and cols
+    # Spritesheet to gif
+    # apply naming conventions
